5551abf4e5313ab62f7babf770e90ae396283b0f
mluthfi3d <mluthfi3d@gmail.com>
2023-09-07 16:46:02 +0700

    Initial Commit

---
+++ ChatSi/ChatSiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatSiApp.swift
+//  ChatSi
+//
+//  Created by Muhammad Luthfi on 07/09/23.
+//
+
+import SwiftUI
+
+@main
+struct ChatSiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
